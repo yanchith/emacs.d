4cf0e455049d09e2ca6f315ead9500dd5e557096
yanchith <yanchi.toth@gmail.com>
2018-09-09 20:19:24 +1000

    Get rid of prelude=mode

--- init.el
+++ init.el
@@ -204,17 +204,6 @@ If Emacs has already finished initialization, also eval FORM immediately."
     (when after-init-time
       (eval form))))
 
-;; Emacs in macOS already has fullscreen support
-;; Emacs has a similar built-in command in 24.4
-(defun ya/fullscreen ()
-  "Make Emacs window fullscreen.
-This follows freedesktop standards, should work in X servers."
-  (interactive)
-  (if (eq window-system 'x)
-      (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
-                             '(2 "_NET_WM_STATE_FULLSCREEN" 0))
-    (error "Only X server is supported")))
-
 ;; MODE
 
 (message "Init phase: mode")
@@ -222,119 +211,38 @@ This follows freedesktop standards, should work in X servers."
 (require 'easymenu)
 (require 'crux)
 
-(defvar ya/ya-mode-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "C-c o") 'crux-open-with)
-    ;; mimic popular IDEs binding, note that it doesn't work in a terminal session
-    (define-key map (kbd "C-a") 'crux-move-beginning-of-line)
-    (define-key map [(shift return)] 'crux-smart-open-line)
-    (define-key map (kbd "M-o") 'crux-smart-open-line)
-    (define-key map [(control shift return)] 'crux-smart-open-line-above)
-    (define-key map [(control shift up)]  'move-text-up)
-    (define-key map [(control shift down)]  'move-text-down)
-    (define-key map [(meta shift up)]  'move-text-up)
-    (define-key map [(meta shift down)]  'move-text-down)
-    (define-key map (kbd "C-c n") 'crux-cleanup-buffer-or-region)
-    (define-key map (kbd "C-c f") 'crux-recentf-ido-find-file)
-    (define-key map (kbd "C-M-z") 'crux-indent-defun)
-    (define-key map (kbd "C-c u") 'crux-view-url)
-    (define-key map (kbd "C-c e") 'crux-eval-and-replace)
-    (define-key map (kbd "C-c s") 'crux-swap-windows)
-    (define-key map (kbd "C-c D") 'crux-delete-file-and-buffer)
-    (define-key map (kbd "C-c d") 'crux-duplicate-current-line-or-region)
-    (define-key map (kbd "C-c M-d") 'crux-duplicate-and-comment-current-line-or-region)
-    (define-key map (kbd "C-c r") 'crux-rename-buffer-and-file)
-    (define-key map (kbd "C-c t") 'crux-visit-term-buffer)
-    (define-key map (kbd "C-c k") 'crux-kill-other-buffers)
-    (define-key map (kbd "C-c TAB") 'crux-indent-rigidly-and-copy-to-clipboard)
-    (define-key map (kbd "C-c I") 'crux-find-user-init-file)
-    (define-key map (kbd "C-c S") 'crux-find-shell-init-file)
-    ;; extra prefix for projectile
-    (define-key map (kbd "s-p") 'projectile-command-map)
-    (define-key map (kbd "C-c p") 'projectile-command-map)
-    ;; make some use of the Super key
-    (define-key map (kbd "s-r") 'crux-recentf-ido-find-file)
-    (define-key map (kbd "s-j") 'crux-top-join-line)
-    (define-key map (kbd "s-k") 'crux-kill-whole-line)
-    (define-key map (kbd "s-m m") 'magit-status)
-    (define-key map (kbd "s-m l") 'magit-log)
-    (define-key map (kbd "s-m f") 'magit-log-buffer-file)
-    (define-key map (kbd "s-m b") 'magit-blame)
-    (define-key map (kbd "s-o") 'crux-smart-open-line-above)
+;;;;;;;;;;;;;;;;;;;;;;;;
 
-    map)
-  "Keymap for Prelude mode.")
+(global-set-key map (kbd "C-a") 'crux-move-beginning-of-line)
 
-(defun ya/ya-mode-add-menu ()
-  "Add a menu entry for `ya-mode' under Tools."
-  (easy-menu-add-item nil
-                      '("Tools")
-                      '("Ya"
-                        ("Files"
-                         ["Open with..." crux-open-with]
-                         ["Re-open as root" crux-reopen-as-root]
-                         ["Delete file and buffer" crux-delete-file-and-buffer]
-                         ["Rename buffer and file" crux-rename-buffer-and-file]
-                         ["Find init file" crux-find-user-init-file]
-                         ["Find custom file" crux-find-user-custom-file]
-                         ["Find shell config file" crux-find-shell-init-file])
+(global-set-key map [(shift return)] 'crux-smart-open-line)
+(global-set-key map (kbd "M-o") 'crux-smart-open-line)
+(global-set-key map [(control shift return)] 'crux-smart-open-line-above)
+(global-set-key map (kbd "s-o") 'crux-smart-open-line-above)
+(global-set-key map (kbd "C-c d") 'crux-duplicate-current-line-or-region)
+(global-set-key map (kbd "C-c M-d") 'crux-duplicate-and-comment-current-line-or-region)
+(global-set-key map (kbd "s-j") 'crux-top-join-line)
+(global-set-key map (kbd "s-k") 'crux-kill-whole-line)
 
-                        ("Buffers"
-                         ["Clean up buffer or region" crux-cleanup-buffer-or-region]
-                         ["Kill other buffers" crux-kill-other-buffers])
+(global-set-key map (kbd "C-c f") 'crux-recentf-ido-find-file)
+(global-set-key map (kbd "s-r") 'crux-recentf-ido-find-file)
+(global-set-key map (kbd "C-c r") 'crux-rename-buffer-and-file)
+(global-set-key map (kbd "C-c k") 'crux-kill-other-buffers)
+(global-set-key map (kbd "C-c I") 'crux-find-user-init-file)
+(global-set-key map (kbd "C-c S") 'crux-find-shell-init-file)
 
-                        ("Editing"
-                         ["Go to beginning of line" crux-move-beginning-of-line]
-                         ["Kill line" crux-smart-kill-line]
-                         ["Kill whole line" crux-kill-whole-line]
-                         ["Insert empty line below" crux-smart-open-line]
-                         ["insert empty line above" crux-smart-open-line-above]
-                         ["Move up" move-text-up]
-                         ["Move down" move-text-down]
-                         ["Duplicate line or region" crux-duplicate-current-line-or-region]
-                         ["Indent rigidly and copy to clipboard" crux-indent-rigidly-and-copy-to-clipboard]
-                         ["Indent defun" crux-indent-defun]
-                         ["Insert date" crux-insert-date]
-                         ["Eval and replace" crux-eval-and-replace]
-                         )
+(global-set-key map [(control shift up)]  'move-text-up)
+(global-set-key map [(control shift down)]  'move-text-down)
+(global-set-key map [(meta shift up)]  'move-text-up)
+(global-set-key map [(meta shift down)]  'move-text-down)
 
-                        ("Windows"
-                         ["Swap windows" crux-swap-windows])
+(global-set-key map (kbd "s-p") 'projectile-command-map)
+(global-set-key map (kbd "C-c p") 'projectile-command-map)
 
-                        ("General"
-                         ["Visit term buffer" crux-visit-term-buffer]
-                         ["Search in Google" prelude-google]
-                         ["View URL" crux-view-url]))
-                      "Search Files (Grep)...")
-
-  (easy-menu-add-item nil '("Tools") '("--") "Search Files (Grep)..."))
-
-(defun ya/ya-mode-remove-menu ()
-  "Remove `prelude-mode' menu entry."
-  (easy-menu-remove-item nil '("Tools") "Ya")
-  (easy-menu-remove-item nil '("Tools") "--"))
-
-(define-minor-mode ya/ya-mode
-  "Minor mode to consolidate the extensions.
-
-\\{ya/ya-mode-map}"
-  :lighter " Pre"
-  :keymap ya/ya-mode-map
-  (if ya/ya-mode
-      ;; on start
-      (ya/ya-mode-add-menu)
-    ;; on stop
-    (ya/ya-mode-remove-menu)))
-
-(define-globalized-minor-mode ya-global-mode ya/ya-mode ya-on)
-
-(defun ya-on ()
-  "Turn on `ya-mode'."
-  (prelude-mode +1))
-
-(defun ya-off ()
-  "Turn off `ya-mode'."
-  (prelude-mode -1))
+(global-set-key map (kbd "s-m m") 'magit-status)
+(global-set-key map (kbd "s-m l") 'magit-log)
+(global-set-key map (kbd "s-m f") 'magit-log-buffer-file)
+(global-set-key map (kbd "s-m b") 'magit-blame)
 
 ;; EDITOR
 
@@ -624,12 +532,12 @@ The body of the advice is in BODY."
 
 (require 'compile)
 (setq
-  ;; Just save before compiling
-  compilation-ask-about-save nil
-  ;; Just kill old compile processes before starting the new one
-  compilation-always-kill t
-  ;; Automatically scroll to first error
-  compilation-scroll-output 'first-error)
+ ;; Just save before compiling
+ compilation-ask-about-save nil
+ ;; Just kill old compile processes before starting the new one
+ compilation-always-kill t
+ ;; Automatically scroll to first error
+ compilation-scroll-output 'first-error)
 
 ;; Colorize output of Compilation Mode, see
 ;; http://stackoverflow.com/a/3072831/355252
@@ -696,7 +604,7 @@ and file 'filename' will be opened and cursor set on line 'linenumber'"
 
 ;; Start proced in a similar manner to dired
 (unless (eq system-type 'darwin)
-    (global-set-key (kbd "C-x p") 'proced))
+  (global-set-key (kbd "C-x p") 'proced))
 
 ;; Start eshell or switch to it if it's active.
 (global-set-key (kbd "C-x m") 'eshell)
