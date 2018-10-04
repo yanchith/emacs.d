95c992ddf654a6af0a21b2247d70201d5fc29c63
yanchith <yanchi.toth@gmail.com>
2018-10-04 11:12:21 +1000

    Add ivy, swiper, counsel and company

--- init.el
+++ init.el
@@ -187,11 +187,53 @@
 (setq undo-tree-auto-save-history t)
 (global-undo-tree-mode)
 
-;; TODO: these probably require more configuring
+;; Set up ivy, swiper & counsel
+
 (ya/install 'ivy)
 (ya/install 'swiper)
 (ya/install 'counsel)
+
+(require 'ivy)
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+
+(setq projectile-completion-system 'ivy)
+
+;; swiper provides enhanced buffer search
+
+(global-set-key "\C-s" 'swiper)
+
+;; counsel supercharges a lot of commands with some ivy magic
+
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c a") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+
 (ya/install 'company)
+(require 'company)
+
+(setq company-idle-delay 0.5)
+(setq company-show-numbers t)
+(setq company-tooltip-limit 10)
+(setq company-minimum-prefix-length 2)
+(setq company-tooltip-align-annotations t)
+;; invert the navigation direction if the the completion popup-isearch-match
+;; is displayed on top (happens near the bottom of windows)
+(setq company-tooltip-flip-when-above t)
+
+(global-company-mode 1)
 
 ;; Revert buffers automatically when underlying files are changed externally
 (global-auto-revert-mode t)
@@ -421,6 +463,12 @@
   (ya/install 'exec-path-from-shell)
   (require 'exec-path-from-shell)
   (exec-path-from-shell-initialize)
+
+  ;; Set option/alt -> meta and cmd -> super
+  (setq mac-option-key-is-meta t)
+  (setq mac-command-key-is-meta nil)
+  (setq mac-command-modifier 'super)
+  (setq mac-option-modifier 'meta)
 
   ;; Set fn as function key
   (setq ns-function-modifier 'hyper))
