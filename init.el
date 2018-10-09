c5e4ee209bd54a75e9fb1f4357c9c637893978d8
yanchith <yanchi.toth@gmail.com>
2018-10-09 11:15:14 +1000

    Remove more cruft, add audit TODOs

--- init.el
+++ init.el
@@ -16,6 +16,8 @@
 
 (defvar ya/dir-savefile (expand-file-name "savefile" ya/dir-root)
   "This directory stores all automatically generated save/history files.")
+(unless (file-exists-p ya/dir-savefile)
+  (make-directory ya/dir-savefile))
 
 (defvar ya/dir-packages-elpa (expand-file-name "packages-elpa" ya/dir-root)
   "This directory stores all downloaded packages.")
@@ -40,22 +42,6 @@
   (when (not (package-installed-p pkg))
     (package-install pkg)))
 
-;; TODO: configure these packages
-;; --JSON
-;; json-mode
-
-;; --Web (HTML + CSS + JS)
-;; web-mode
-
-;; --JavaScript
-;; js2-mode
-
-;; --TypeScript
-;; tide
-
-;; --Haskell
-;; haskell-mode
-
 ;; Reclaim some screen real-estate
 (when (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
@@ -66,13 +52,16 @@
 ;; Disable cursor blinking
 (blink-cursor-mode -1)
 
-;; Disable the annoying sounds
+;; Always highlight the current line
+(global-hl-line-mode +1)
+
+;; Disable sounds
 (setq ring-bell-function 'ignore)
 
 ;; Disable startup screen
 (setq inhibit-startup-screen t)
 
-;; Add nice scrolling
+;; Add nice scrolling, TODO: audit
 (setq scroll-margin 0
       scroll-conservatively 100000
       scroll-preserve-screen-position 1)
@@ -82,19 +71,18 @@
 (column-number-mode t)
 (size-indication-mode t)
 
-;; Enable y/n answers
+;; Enable y/n answers, TODO: audit
 (fset 'yes-or-no-p 'y-or-n-p)
 
 ;; Add more useful frame title, Show either a file or a
 ;; buffer name (if the buffer isn't visiting a file)
 (setq frame-title-format
       '("" invocation-name ""
-        (:eval (if (buffer-file-name)
-                   (abbreviate-file-name (buffer-file-name))
-                 "%b"))))
+        (:eval (if (buffer-fiLe-name)
+                   (abbreviate-file-name (buffer-file-name)) "%b"))))
 
 (ya/install 'doom-themes)
-(load-theme 'doom-one t)
+(load-theme 'doom-one t) ;; TODO: audit (there are theme-specific settings)
 (setq doom-themes-enable-bold t
       doom-themes-enable-italic t)
 
@@ -115,8 +103,6 @@
 (global-set-key (kbd "s-j") 'crux-top-join-line)
 (global-set-key (kbd "s-k") 'crux-kill-whole-line)
 
-(global-set-key (kbd "C-c f") 'crux-recentf-ido-find-file)
-(global-set-key (kbd "s-r") 'crux-recentf-ido-find-file)
 (global-set-key (kbd "C-c r") 'crux-rename-buffer-and-file)
 (global-set-key (kbd "C-c k") 'crux-kill-other-buffers)
 (global-set-key (kbd "C-c I") 'crux-find-user-init-file)
@@ -135,8 +121,6 @@
 (global-set-key (kbd "C-c p") 'projectile-command-map)
 
 (ya/install 'magit)
-(global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 (global-set-key (kbd "s-m m") 'magit-status)
 (global-set-key (kbd "s-m l") 'magit-log)
 (global-set-key (kbd "s-m f") 'magit-log-buffer-file)
@@ -144,10 +128,7 @@
 
 (ya/install 'git-timemachine)
 
-;; EDITOR
-
-(message "Init phase: editor")
-
+;; TODO: audit
 ;; Death to tabs! However, tabs historically indent to the next
 ;; 8-character offset; specifying anything else will cause *mass*
 ;; confusion, as it will change the appearance of every existing file.
@@ -179,8 +160,7 @@
 (setq undo-tree-auto-save-history t)
 (global-undo-tree-mode)
 
-;; Set up ivy, swiper & counsel
-
+;; Set up ivy, swiper and counsel
 (ya/install 'ivy)
 (ya/install 'swiper)
 (ya/install 'counsel)
@@ -190,113 +170,76 @@
 (setq ivy-use-virtual-buffers t)
 (setq enable-recursive-minibuffers t)
 
-(global-set-key (kbd "C-c C-r") 'ivy-resume)
-
+;; Integrate ivy with projectile
 (setq projectile-completion-system 'ivy)
 
-;; swiper provides enhanced buffer search
-
+;; swiper provides enhanced buffer search, replace i-search with swiper
 (global-set-key "\C-s" 'swiper)
 
 ;; counsel supercharges a lot of commands with some ivy magic
-
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-(global-set-key (kbd "<f1> f") 'counsel-describe-function)
-(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-(global-set-key (kbd "<f1> l") 'counsel-find-library)
-(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
-(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
 (global-set-key (kbd "C-c g") 'counsel-git)
 (global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c a") 'counsel-ag)
 (global-set-key (kbd "C-x l") 'counsel-locate)
-(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
 
 (ya/install 'company)
 (require 'company)
 
-(setq company-idle-delay 0.5)
+;; Set up completetions in text, TODO: audit
+(setq company-idle-delay 0.3)
 (setq company-show-numbers t)
 (setq company-tooltip-limit 10)
 (setq company-minimum-prefix-length 2)
 (setq company-tooltip-align-annotations t)
-;; invert the navigation direction if the the completion popup-isearch-match
-;; is displayed on top (happens near the bottom of windows)
-(setq company-tooltip-flip-when-above t)
-
 (global-company-mode 1)
+
+;; avy allows us to effectively navigate to visible things
+(ya/install 'avy)
+(require 'avy)
+(setq avy-background t)
+(setq avy-style 'at-full)
+(global-set-key (kbd "s-.") 'avy-goto-word-or-subword-1)
+
+(ya/install 'multiple-cursors)
+(require 'multiple-cursors)
+(global-set-key (kbd "C-c m c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+;; TODO mc/mark-all-like-this
+
+(ya/install 'editorconfig)
+(require 'editorconfig)
+(editorconfig-mode 1)
+
+(ya/install 'expand-region)
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+;; Improve window navigation with ace-window
+(ya/install 'ace-window)
+(global-set-key [remap other-window] 'ace-window)
 
 ;; Revert buffers automatically when underlying files are changed externally
 (global-auto-revert-mode t)
 
-;; Hippie expand is dabbrev expand on steroids
-(setq hippie-expand-try-functions-list '(try-expand-dabbrev
-                                         try-expand-dabbrev-all-buffers
-                                         try-expand-dabbrev-from-kill
-                                         try-complete-file-name-partially
-                                         try-complete-file-name
-                                         try-expand-all-abbrevs
-                                         try-expand-list
-                                         try-expand-line
-                                         try-complete-lisp-symbol-partially
-                                         try-complete-lisp-symbol))
-
 ;; Smart tab behavior - indent or complete
 (setq tab-always-indent 'complete)
 
-;; saveplace remembers your location in a file when saving files
+;; saveplace remembers your location in a file when saving files activate it
+;; for all buffers
 (setq save-place-file (expand-file-name "saveplace" ya/dir-savefile))
-;; activate it for all buffers
 (save-place-mode 1)
 
-;; savehist keeps track of some history
-(require 'savehist)
-(setq savehist-additional-variables
-      ;; search entries
-      '(search-ring regexp-search-ring)
-      ;; save every minute
-      savehist-autosave-interval 60
-      ;; keep the home clean
-      savehist-file (expand-file-name "savehist" ya/dir-savefile))
-(savehist-mode +1)
-
-;; save recent files
-(require 'recentf)
-(setq recentf-save-file (expand-file-name "recentf" ya/dir-savefile)
-      recentf-max-saved-items 500
-      recentf-max-menu-items 15
-      ;; disable recentf-cleanup on Emacs start, because it can cause
-      ;; problems with remote files
-      recentf-auto-cleanup 'never)
-
-(defun prelude-recentf-exclude-p (file)
-  "A predicate to decide whether to exclude FILE from recentf."
-  (let ((file-dir (file-truename (file-name-directory file))))
-    (cl-some (lambda (dir)
-               (string-prefix-p dir file-dir))
-             (mapcar 'file-truename (list ya/dir-savefile package-user-dir)))))
-
-(add-to-list 'recentf-exclude 'prelude-recentf-exclude-p)
-
-(recentf-mode +1)
 
 
-(defadvice set-buffer-major-mode (after set-major-mode activate compile)
-  "Set buffer major mode according to `auto-mode-alist'."
-  (let* ((name (buffer-name buffer))
-         (mode (assoc-default name auto-mode-alist 'string-match)))
-    (when (and mode (consp mode))
-      (setq mode (car mode)))
-    (with-current-buffer buffer (if mode (funcall mode)))))
 
-;; highlight the current line
-(global-hl-line-mode +1)
+;; --------------------
+;; TODO audit the rest!
+;; --------------------
 
-;; tramp, for sudo access
-(require 'tramp)
-;; keep in mind known issues with zsh - see emacs wiki
-(setq tramp-default-method "ssh")
+
+
 
 ;; flyspell-mode does spell-checking on the fly as you type
 (require 'flyspell)
@@ -335,20 +278,6 @@
 ;; enable erase-buffer command
 (put 'erase-buffer 'disabled nil)
 
-;; avy allows us to effectively navigate to visible things
-(ya/install 'avy)
-(require 'avy)
-(setq avy-background t)
-(setq avy-style 'at-full)
-(global-set-key (kbd "s-.") 'avy-goto-word-or-subword-1)
-
-(ya/install 'multiple-cursors)
-(require 'multiple-cursors)
-(global-set-key (kbd "C-c m c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-;; TODO mc/mark-all-like-this
-
 ;; dired - reuse current buffer by pressing 'a'
 (put 'dired-find-alternate-file 'disabled nil)
 
@@ -370,9 +299,6 @@
 ;; Clean up obsolete buffers automatically
 (require 'midnight)
 
-;; abbrev config
-(add-hook 'text-mode-hook 'abbrev-mode)
-
 ;; Whitespace-mode config
 (require 'whitespace)
 (setq whitespace-line-column 80)
@@ -385,11 +311,11 @@
 (require 'eshell)
 (setq eshell-directory-name (expand-file-name "eshell" ya/dir-savefile))
 
-;; TODO: what is semanticdb?
+;; TODO: what is semanticdb? audit
 (setq semanticdb-default-save-directory
       (expand-file-name "semanticdb" ya/dir-savefile))
 
-;; TODO: what is this?
+;; TODO: what is this? audit
 (require 'compile)
 (setq
  ;; Just save before compiling
@@ -406,10 +332,6 @@
 (global-diff-hl-mode +1)
 (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
 (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
-
-(ya/install 'editorconfig)
-(require 'editorconfig)
-(editorconfig-mode 1)
 
 ;; Font size
 (global-set-key (kbd "C-+") 'text-scale-increase)
@@ -428,27 +350,15 @@
 
 (define-key 'help-command (kbd "C-i") 'info-display-manual)
 
-;; kill lines backward
+;; kill lines backward, TODO: audit - isn't this the default?
 (global-set-key (kbd "C-<backspace>")
                 (lambda ()
                   (interactive)
                   (kill-line 0)
                   (indent-according-to-mode)))
 
-;; Use hippie-expand instead of dabbrev
-(global-set-key (kbd "M-/") 'hippie-expand)
-
 ;; Replace buffer-menu with ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
-
-(ya/install 'expand-region)
-(require 'expand-region)
-(global-set-key (kbd "C-=") 'er/expand-region)
-
-;; Improve window navigation with ace-window
-(ya/install 'ace-window)
-(global-set-key (kbd "s-w") 'ace-window)
-(global-set-key [remap other-window] 'ace-window)
 
 ;; General programming support
 
@@ -557,3 +467,11 @@
 (setq gc-cons-threshold 800000)
 
 (message "Done!")
+
+;; TODO:
+;; Editing: recentf, savehist
+;; JSON: json-mode
+;; Web (HTML/CSS/JS): web-mode
+;; JavaScript: js2-mode
+;; TypeScript: tide
+;; Haskell: haskell-mode
