7dd0dd7fb2bbee62304f10e8416ed9b458be6bb3
yanchith <yanchi.toth@gmail.com>
2019-03-04 23:43:07 +0100

    Change whitespace-line-column to 100

--- init.el
+++ init.el
@@ -280,7 +280,7 @@
 ;; Whitespace-mode config
 (use-package whitespace
   :config
-  (setq whitespace-line-column 80
+  (setq whitespace-line-column 100
         whitespace-style '(face tabs empty trailing lines-tail))
   (add-hook 'before-save-hook 'whitespace-cleanup nil t)
   (global-whitespace-mode +1))
