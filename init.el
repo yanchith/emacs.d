8f1fb49a01e1fd989369967ec4881f053067b6de
yanchith <yanchi.toth@gmail.com>
2019-07-23 17:03:30 +0200

    Select font based on OS

--- init.el
+++ init.el
@@ -40,7 +40,8 @@
 ;;;; Customize UI
 
 ;; Set the default font
-;; (set-default-font "Liberation Mono-11.5")
+(cond ((eq system-type 'darwin) (set-default-font "Monaco 12"))
+      ((eq system-type 'windows-nt) (set-default-font "Liberation Mono-11")))
 
 ;; Hide all the bars
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
